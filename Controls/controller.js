f47c1721b6624c8144c59e04fbe4c0778102c304
darrenCoding <--get>
2016-03-22 22:48:39 +0800

    fix a bug

--- Controls/controller.js
+++ Controls/controller.js
@@ -12,6 +12,7 @@ var slice = Array.prototype.slice;
 
 var conRoute = module.exports = (function(){
 	function route(req,res,next){
+		console.log(req.url);
 		route.handle(req,res,next)
 	};
 	route.__proto__ = conRoute;
