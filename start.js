87befa3afc4bb8314849da973dd1fa2735fdfc65
darrenCoding <--get>
2016-04-08 00:18:50 +0800

    fix a bug

--- start.js
+++ start.js
@@ -6,12 +6,15 @@ var startFn = require('./index');
 // 默认配置
 var setting = require('./config').mock;
 
+var args = process.argv.slice(2),
+	port = (args[1] && /^\d+$/.test(args[0])) ? parseInt(args[0]) : 8011;
+
 var start = module.exports = function(d,p,cb){
 	var dir = d || process.cwd(),
 		port = p || setting.port;
 	startFn(dir,port,cb)
 }
 
-if(require.main){
-	startFn(null,8011)
+if(require.main.filename === __filename){
+	startFn(null,port)
 }
