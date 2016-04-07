a5a6ce4c978f6ae334637825edd2145bc37401da
darrenCoding <--get>
2016-04-08 01:40:45 +0800

    fix a bug

--- Controls/controller.js
+++ Controls/controller.js
@@ -1,9 +1,11 @@
 
 'use strict';
 
-var util = require('../lib/util');
 var pathReg = require('path-to-regexp');
+var combo = require('static-combo');
+var util = require('../lib/util');
 var setting = require('../config');
+var path = require('path');
 var mock = require('./mock');
 var http = require('http');
 
@@ -20,58 +22,71 @@ var conRoute = module.exports = (function(){
 
 conRoute.handle = function(req,res,next){
 	if(req.url !== '/favicon.ico'){
-		console.log(req.url);
-		util.parse(req);
-		var mapIndex = 0,
-			viaHeaders = [],
-			mapLen = Object.keys(setting.mock.route).length,
-			reg;
-		util.each(setting.mockMap,function(i,host,go){
-			if(util.getType(host) === 'object'){
-				viaHeaders.push(host['host']);
-				res.set('X-Forwarded-For',viaHeaders.join(","));
-				var options = {
-			        host : host['host'],   
-			        method : req.method,
-			        port : host['port'],
-			        path : req.url
-			    };
+		if(!path.extname(req.url)){
+			util.parse(req);
+			var mapIndex = 0,
+				viaHeaders = [],
+				mapLen = Object.keys(setting.mock.route).length,
+				reg;
+			util.each(setting.mockMap,function(i,host,go){
+				if(util.getType(host) === 'object'){
+					viaHeaders.push(host['host']);
+					res.set('X-Forwarded-For',viaHeaders.join(","));
+					var options = {
+				        host : host['host'],   
+				        method : req.method,
+				        port : host['port'],
+				        path : req.url
+				    };
 
-			    var istimeout = setTimeout(function(){
-			    	return next(host['host'] + ":" + host['port'] + " timeout，please check your server");
-			    },3000)
-			    var pReq = http.request(options,function(cRes){
-			    	clearTimeout(istimeout);
-					if(cRes.statusCode <= 400){
-						res.status(cRes.statusCode).set(cRes.headers);
-						cRes.pipe(res);
-					}else{
-						go();
-					}
-				}).on('error',function(e){
-					clearTimeout(istimeout);
-					go();
-				});
-				pReq.end();
-			}else{
-				viaHeaders.push('mock');
-				res.set('X-Forwarded-For',viaHeaders.join(","));
-				for(var attr in setting.mock.route){
-					reg = pathReg(attr);
-					if (setting.mock.route.hasOwnProperty(attr)){
-						if(reg.test(req.url)){
-							mock[getFn(setting.mock.route[attr])](req,res,next,reg);
-							return;
+				    var istimeout = setTimeout(function(){
+				    	return next(host['host'] + ":" + host['port'] + " timeout，please check your server");
+				    },3000)
+				    var pReq = http.request(options,function(cRes){
+				    	clearTimeout(istimeout);
+						if(cRes.statusCode <= 400){
+							res.status(cRes.statusCode).set(cRes.headers);
+							cRes.pipe(res);
 						}else{
-							mapIndex++;
+							go();
+						}
+					}).on('error',function(e){
+						clearTimeout(istimeout);
+						go();
+					});
+					pReq.end();
+				}else{
+					viaHeaders.push('mock');
+					res.set('X-Forwarded-For',viaHeaders.join(","));
+					for(var attr in setting.mock.route){
+						reg = pathReg(attr);
+						if (setting.mock.route.hasOwnProperty(attr)){
+							if(reg.test(req.url)){
+								mock[getFn(setting.mock.route[attr])](req,res,next,reg);
+								return;
+							}else{
+								mapIndex++;
+							}
+						}
+						if(mapIndex === mapLen){
+							next('the url is invalid, please check your url');
 						}
 					}
-					if(mapIndex === mapLen){
-						next('the url is invalid, please check your url');
-					}
 				}
-			}
-		})
+			})
+		}else{
+			combo.config({
+				"base_path" : process.cwd(),
+				"compress" : false
+			})
+			combo(req.url,(err,data,deps) => {
+				if(err){
+                    res.end(err);
+                }else{
+                    res.end(new Buffer(data));
+                }
+	        }); 
+		}
 	}
 }
 
